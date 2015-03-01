75adee952fde99d31a05b9e0af91364619212715
patte <patrick.recher@gmail.com>
2015-03-01 15:02:18 +0100

    version bump 0.0.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'patte:autoform-map',
   summary: 'Edit location coordinates with autoForm',
-  version: '0.0.3',
+  version: '0.0.4',
   git: 'https://github.com/patte/meteor-autoform-map'
 });
 
